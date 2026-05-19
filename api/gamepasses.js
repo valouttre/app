11e507155aa2946c6e2370a6cad0d933d768c6d8
valouttre <deroxi.ytb@gmail.com>
2026-05-19 12:22:37 +0200

    Update gamepasses.js

--- api/gamepasses.js
+++ api/gamepasses.js
@@ -2,27 +2,21 @@ export default async function handler(req, res) {
 
     const userId = req.query.userId;
 
-    if (!userId) {
-        return res.status(400).json({
-            error: "Missing userId"
-        });
-    }
-
     try {
 
         const response = await fetch(
-            `https://catalog.roblox.com/v1/search/items/details?Category=3&CreatorTargetId=${userId}&CreatorType=User`
+            `https://catalog.roblox.com/v1/search/items/details?Category=1&Subcategory=7&CreatorTargetId=${userId}&CreatorType=User`
         );
 
         const data = await response.json();
 
         res.setHeader("Access-Control-Allow-Origin", "*");
 
-        return res.status(200).json(data);
+        res.status(200).json(data);
 
     } catch (err) {
 
-        return res.status(500).json({
+        res.status(500).json({
             error: err.message
         });
 
